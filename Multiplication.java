4b2ed0bda4d45138fc8373921c9f74fdbfbab325
veeravenkatadeviramprasadballa <152369525+veeravenkatadeviramprasadballa@users.noreply.github.com>
2024-08-09 11:08:28 +0530

    import java.util.Scanner; public class Arithmeticseriessum{     public static void main(String[] args) {
            int a,b,c;
            Scanner sc=new Scanner(System.in);
            a=sc.nextInt();
            b=sc.nextInt();
            c=a*b;
            System.out.println(c);
            sc.close();
    
    
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Arithmeticseriessum{
+    public static void main(String[] args) {
+        
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a*b;
+        System.out.println(c);
+        sc.close();
+
+
+    }
+}
